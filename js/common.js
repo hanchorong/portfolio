ed624647dbebd68116e3257d39e0d48b5e2b2fd1
hanchorong <hancrong@gmail.com>
2023-07-07 14:54:28 +0900

    nav, footer

--- js/common.js
+++ js/common.js
@@ -1,17 +1,40 @@
-const hamburgerBtn = document.querySelector(".hamburgerBtn");
-// const mobile_nav_div = document.querySelector(".mobile_nav_div");
+//nav
+const $hamburgerBtn = document.querySelector(".hamburgerBtn");
 const mobile_ul = document.querySelector(".mobile_ul");
 const btnStatus = document.querySelector(".hamburgerBtn>i");
 
-hamburgerBtn.addEventListener("click", () => {
+$hamburgerBtn.addEventListener("click", () => {
   mobile_ul.classList.add("display_block");
   mobile_ul.classList.remove("display_none");
 
   if (btnStatus.className === "fa-solid fa-bars") {
-    btnStatus.className = "fa-solid fa-xmark";
+    btnStatus.className = "fa-regular fa-circle-xmark";
   } else {
     btnStatus.className = "fa-solid fa-bars";
     mobile_ul.classList.add("display_none");
     mobile_ul.classList.remove("display_block");
   }
 });
+
+//scroll top
+const $topButton = document.querySelector(".topBtn");
+const section_two = window.innerHeight;
+
+function checkWindowHeight() {
+  const scroll_Y = window.scrollY;
+
+  if (scroll_Y > section_two) {
+    $topButton.classList.remove("display_none");
+    $topButton.classList.add("display_block");
+  } else {
+    $topButton.classList.remove("display_block");
+    $topButton.classList.add("display_none");
+  }
+}
+
+window.addEventListener("scroll", checkWindowHeight);
+window.addEventListener("resize", checkWindowHeight);
+
+$topButton.onclick = () => {
+  window.scrollTo({ top: 0, behavior: "smooth" });
+};
