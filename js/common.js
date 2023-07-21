db6c7b95453398f2e702112525751799f324e661
hanchorong <hancrong@gmail.com>
2023-07-21 14:49:40 +0900

    scroll evnet

--- js/common.js
+++ js/common.js
@@ -16,9 +16,12 @@ $hamburgerBtn.addEventListener("click", () => {
   }
 });
 
-//scroll top
+//scroll event
 const $topButton = document.querySelector(".topBtn");
 const section_two = window.innerHeight;
+const $mainArticle = document.querySelector(".main_wrap article");
+const $about_back = document.querySelector(".about_back");
+const $about_wrap = document.querySelector(".about_wrap");
 
 function checkWindowHeight() {
   const scroll_Y = window.scrollY;
@@ -30,6 +33,15 @@ function checkWindowHeight() {
     $topButton.classList.remove("display_block");
     $topButton.classList.add("display_none");
   }
+
+  if (scroll_Y + 400 > section_two) {
+    $mainArticle.classList.add("fadeout");
+    $about_wrap.classList.add("fadeIn");
+  } else {
+    $mainArticle.classList.remove("fadeout");
+  }
+
+  $about_back.style.transform = `translateY(${-scroll_Y}px) translateZ(0px)`;
 }
 
 window.addEventListener("scroll", checkWindowHeight);
